9a9eada9bf44efb3e30d8d4b3f14e79de0b3188f
rienierpatron <rienierpatron@gmail.com>
2016-08-10 11:18:18 +0800

    mod

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 var app = angular.module('ms1', []);
 app.controller('MainCtrl',['$scope', function($scope) {
   $scope.nav = function(event, path) {
-  		$('.navbar-right li').removeClass('active');
+  		$('.mainNav').removeClass('active');
   		console.log(event);
 	    $scope.filePath = path;
 	}; 
